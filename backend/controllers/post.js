8d9d02c6769193db746ed38f19013344effa3f2c
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-11 17:00:13 +0600

    create the first post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,7 +1,23 @@
 const Post = require("../models/post");
+const User = require("../models/user");
 exports.createPost = async (req, res) => {
   try {
-    
+    const newPostData = {
+      caption:req.body.caption,
+      image:{
+        public_id:"cloudinary_public_id",
+        url:"cloudinary_url"
+      },
+      owner:req.user._id
+    }
+    const post = await Post.create(newPostData);
+    const user = await User.findById(req.user._id);
+    user.posts.push(post._id);
+    await user.save();
+    res.status(201).json({
+      success:true,
+      post
+    })
   } catch (error) {
     res.status(500).json({
       success: true,
