a43c09f77c6528b1fcab2955ab76eb8c47500c91
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-12 16:36:56 +0600

    update caption

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const {createPost, likeOrDislike, deletePost, getPostOfFollowing} = require('../controllers/post');
-const {isAuthenticated} = require('../middlewares/auth')
+const express = require("express");
+const {
+  createPost,
+  likeOrDislike,
+  deletePost,
+  getPostOfFollowing,
+  updateCaption,
+} = require("../controllers/post");
+const { isAuthenticated } = require("../middlewares/auth");
 const router = express.Router();
-router.route("/post/upload").post(isAuthenticated,createPost);
-router.route("/post/:id").get(isAuthenticated,likeOrDislike).delete(isAuthenticated,deletePost);
-router.route("/posts").get(isAuthenticated,getPostOfFollowing);
+router.route("/post/upload").post(isAuthenticated, createPost);
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeOrDislike)
+  .put(isAuthenticated, updateCaption)
+  .delete(isAuthenticated, deletePost);
+router.route("/posts").get(isAuthenticated, getPostOfFollowing);
 module.exports = router;
