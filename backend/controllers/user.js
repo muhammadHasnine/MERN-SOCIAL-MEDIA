5998d3fe70390d6db4d98fb71657a8ec5e723ace
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-12 18:25:55 +0600

    User Profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -203,3 +203,18 @@ exports.deleteMyProfile = async(req,res)=>{
     });
   }
 }
+//Get My Profile
+exports.myProfile = async(req,res)=>{
+  try {
+    const user = await User.findById(req.user._id).populate("posts");
+    res.status(200).json({
+      success:true,
+      user
+    })
+  } catch (error) {
+    res.status(500).json({
+      success:false,
+      message:error.message
+    });
+  }
+}
