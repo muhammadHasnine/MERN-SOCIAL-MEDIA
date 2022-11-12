27ec77463145eaf2c06d41ec4f2867b51d1638ee
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-12 08:13:56 +0600

    Get post of Following

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -91,3 +91,23 @@ exports.likeOrDislike = async(req,res)=>{
     })
   }
 }
+//Get Post of Following
+exports.getPostOfFollowing = async(req,res)=>{
+  try {
+    const user = await User.findById(req.user._id);
+    const posts = await Post.find({
+      owner:{
+        $in:user.following
+      }
+    })
+    res.status(200).json({
+      success:true,
+      posts
+    })
+  } catch (error) {
+    res.status(500).json({
+      success:false,
+      message:error.message
+    })
+  }
+}
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const {createPost, likeOrDislike, deletePost} = require('../controllers/post');
+const {createPost, likeOrDislike, deletePost, getPostOfFollowing} = require('../controllers/post');
 const {isAuthenticated} = require('../middlewares/auth')
 const router = express.Router();
 router.route("/post/upload").post(isAuthenticated,createPost);
 router.route("/post/:id").get(isAuthenticated,likeOrDislike).delete(isAuthenticated,deletePost);
+router.route("/posts").get(isAuthenticated,getPostOfFollowing);
 module.exports = router;
