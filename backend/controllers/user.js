b440231422e8804b0b11ea87035f3f67bec6677a
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-12 16:21:16 +0600

    update profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -147,11 +147,34 @@ exports.updatePassword = async(req,res)=>{
   return res.status(200).json({
     success:true,
     message:"Password Updated"
-  })
+  });
 } catch (error) {
     res.status(500).json({
       success:false,
       message:error.message
-    })
+    });
+  }
+}
+//Update Profile
+exports.updateProfile = async(req,res)=>{
+  try {
+    const user = await User.findById(req.user._id);
+    const {name,email}=req.body
+    if(name){
+      user.name = name;
+    }
+    if(email){
+      user.email = email;
+    }
+    await user.save();
+    res.status(200).json({
+      success:true,
+      message:"Profile updated"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success:false,
+      message:error.message
+    });
   }
 }
