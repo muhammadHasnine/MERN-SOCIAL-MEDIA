17ce717b23b84eb327dbe40948cad8d6f4a54b52
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-15 09:54:08 +0600

    Delete comment

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -150,6 +150,7 @@ exports.commentOnPost = async(req,res)=>{
         message:"Post not found"
       });
     }
+    //Checking if comment exist
     let commentIndex = -1;
     post.comments.forEach((item,index)=>{
       if(item.user.toString() === req.user._id.toString()){
@@ -172,6 +173,52 @@ exports.commentOnPost = async(req,res)=>{
       return res.status(200).json({
         success:true,
         message:"Comment added"
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success:false,
+      message:error.message
+    })
+  }
+}
+exports.deleteComment = async(req,res)=>{
+  try {
+    const post = await Post.findById(req.params.id);
+    if(!post){
+      return res.status(404).json({
+        success:false,
+        message:"Post not found"
+      });
+    }
+    //Checking if owner want to delete
+    if(post.owner.toString() === req.user._id.toString()){
+      if(req.body.commentId === undefined){
+        return res.status(400).json({
+          success:false,
+          message:"comment Id is required"
+        });
+      }
+      post.comments.forEach((item,index)=>{
+        if(item._id.toString() === req.body.commentId.toString()){
+          return post.comments.splice(index,1)
+        }
+      })
+      await post.save();
+      return res.status(200).json({
+        success:true,
+        message:"Selected comment has deleted"
+      })
+    }else{ 
+      post.comments.forEach((item,index)=>{
+        if(item.user.toString() === req.user._id.toString()){
+          return  post.comments.splice(index,1);
+        }
+      })
+      await post.save();
+      return res.status(200).json({
+        success:true,
+        message:"Your comment has deleted"
       })
     }
   } catch (error) {
