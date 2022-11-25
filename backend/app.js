a6073cf308ea1a6be9c4cd1fa431296447b3b666
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-25 23:46:10 +0600

    cors bug

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,13 @@ const post = require('./routes/post');
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const app = express();
+app.use(cors())
 //Config for local
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config({path:"backend/config/config.env"})
 }
 //Using Middlewares
-app.use(cors())
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
