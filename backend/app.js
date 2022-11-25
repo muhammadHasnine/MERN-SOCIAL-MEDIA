32bad07a6c8147ceb467fb3c88aaa4d7c1fda8db
muhammadHasnine <mdhasninesheak@gmail.com>
2022-11-25 22:21:25 +0600

    add cors

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const user = require('./routes/user');
 const post = require('./routes/post');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 const app = express();
 //Config for local
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config({path:"backend/config/config.env"})
 }
 //Using Middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
