73bedf92f942dd415581ada6538e06defbd0404a
muhammadHasnine <mdhasninesheak@gmail.com>
2022-12-01 10:09:55 +0600

    cookie option

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -26,8 +26,7 @@ exports.register = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      sameSite:"none",
-      secure:true
+      sameSite:"lax"
       
     };
     res.status(201).cookie("token", token, options).json({
