d4ea126b9028766555953247528ecda2ba6ae582
muhammadHasnine <mdhasninesheak@gmail.com>
2022-12-01 16:07:58 +0600

    origin

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,9 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const app = express();
 app.use(cors({
-    origin:"http/",
-    credentials:true
+    origin:"https://stately-elf-873fbd.netlify.app",
+    credentials:true,
+    
 }))
 //Config for local
 if(process.env.NODE_ENV !== 'production'){
