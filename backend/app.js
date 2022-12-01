702ef7156e1e00c438088d03c0a25b2e5f765547
muhammadHasnine <mdhasninesheak@gmail.com>
2022-12-01 19:05:17 +0600

    cors bug fixed

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const app = express();
 app.use(cors({
-    origin:"https://stately-elf-873fbd.netlify.app",
+    origin:"http://localhost:3000",
+    allowedHeaders:['Content-Type', 'Authorization'],
     credentials:true,
     
 }))
